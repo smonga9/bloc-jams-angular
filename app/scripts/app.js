f3d429836bcbbd82a21f929c2894eae7bae41e3c
Stutay Monga <Stutay.Monga9@gmail.com>
2017-08-21 17:28:00 -0400

    Updated Assignment 3

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,27 +1,29 @@
 (function() {
     function config($stateProvider, $locationProvider) {
-      $locationProvider
-        .html5Mode({
-            enabled: true,
-            requireBase: false
-        });
-      }
+        $locationProvider
+         .html5Mode({
+             enabled: true,
+             requireBase: false
+         });
 
-        angular
-               .module('blocJams', ['ui.router'])
-               .config(config);
-       })();
-
-   $stateProvider
+         $stateProvider
          .state('landing', {
              url: '/',
              templateUrl: '/templates/landing.html'
-           })
-           .state('album', {
-               url: '/album',
-               templateUrl: '/templates/album.html'
-           });
-           .state('collection', {
+         })
+
+         .state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html'
+         })
+
+         .state('collection', {
            url: '/collection',
            templateUrl: '/templates/collection.html'
          });
+    }
+
+       angular
+         .module('blocJams', ['ui.router'])
+         .config(config);
+       })();
