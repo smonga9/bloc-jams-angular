7b6ec8fec6de29cdeaa90c02d40f9a03bd2e43b4
Stutay Monga <Stutay.Monga9@gmail.com>
2017-08-30 05:47:55 -0500

    Assignment 7 Completed

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -2,7 +2,6 @@
   function AlbumCtrl(Fixtures, SongPlayer) {
     this.albumData = Fixtures.getAlbum();
     this.songPlayer = SongPlayer;
-    console.log("album data", this.albumData);
   }
 
      angular
