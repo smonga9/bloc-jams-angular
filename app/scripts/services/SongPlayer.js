8890d9a8237eec5c0029636bb68b69b4d2506f53
Stutay Monga <Stutay.Monga9@gmail.com>
2017-09-02 12:56:31 -0400

    Checkpoint 8 Completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,77 +1,97 @@
 (function() {
-     function SongPlayer() {
+     function SongPlayer(Fixtures) {
          var SongPlayer = {};
 
-   /**
-            * @desc Current Buzz object audio file
-            * @type {Object}
-            */
+         var currentAlbum = Fixtures.getAlbum();
 
-         var currentSong = null;
-
-   /**
-            * @desc Buzz object audio file
-            * @type {Object}
-            */
+         /**
+          * @desc Buzz object audio file
+          * @type {Object}
+          */
          var currentBuzzObject = null;
-            /**
-            * @function
-                * @desc Stops currently playing song and loads new audio file as currentBuzzObject
-                * @param {Object} song
-                */
-        var setSong = function(song) {
+
+         /**
+          * @function
+          * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+          * @param {Object} song
+          */
+         var setSong = function(song) {
              if (currentBuzzObject) {
                  currentBuzzObject.stop();
-                 currentSong.playing = null;
+                 SongPlayer.currentSong.playing = null;
              }
 
-               currentBuzzObject = new buzz.sound(song.audioUrl, {
-                   formats: ['mp3'],
-                   preload: true
-               });
+             currentBuzzObject = new buzz.sound(song.audioUrl, {
+                 formats: ['mp3'],
+                 preload: true
+             });
 
-                currentSong = song;
-            };
+             SongPlayer.currentSong = song;
+         };
 
-            /**
-            * @function playSong
-            * @desc Plays the currentBuzzObject and sets the property of the song Object to true
-            * @param {Object} song
-            */
-            var playSong = function(song){
-                currentBuzzObject.play();
-                song.playing = true;
-            }
+         /**
+          * @function playSong
+          * @desc Plays the currentBuzzObject and sets the property of the song Object to true
+          * @param {Object} song
+          */
+         var playSong = function(song) {
+             currentBuzzObject.play();
+             song.playing = true;
+         }
 
-            /**
-         * @function SongPlayer.play
-         * @desc Public method that takes a song object parameter. If the buzz object Song is not the same as the current
-         * then a new song will load and play. If the buzz object Song is the same, and if it is paused, then the song will play.
-         * @param {Object} song
-         */
+         var getSongIndex = function(song) {
+             return currentAlbum.songs.indexOf(song);
+         };
 
+         /**
+          * @desc Current Buzz object audio file
+          * @type {Object}
+          */
+         SongPlayer.currentSong = null;
+
+
+         /**
+          * @function SongPlayer.play
+          * @desc Public method that takes a song object parameter. If the buzz object Song is not the same as the current
+          * then a new song will load and play. If the buzz object Song is the same, and if it is paused, then the song will play.
+          * @param {Object} song
+          */
          SongPlayer.play = function(song) {
-             if (currentSong !== song) {
+             song = song || SongPlayer.currentSong;
+             if (SongPlayer.currentSong !== song) {
                  setSong(song);
                  playSong(song);
-
-             } else if (currentSong === song) {
+             } else if (SongPlayer.currentSong === song) {
                  if (currentBuzzObject.isPaused()) {
-                    playSong(song);
+                     playSong(song);
                  }
              }
          };
 
          /**
-            * @function SongPlayer.pause
-            * @desc Public method. Takes a song object parameter. Pauses the currently playing Buzz Object
-            * and sets the song's 'playing' attribute to false.
-            * @param {Object} song
-            */
-
+          * @function SongPlayer.pause
+          * @desc Public method. Takes a song object parameter. Pauses the currently playing Buzz Object
+          * and sets the song's 'playing' attribute to false.
+          * @param {Object} song
+          */
          SongPlayer.pause = function(song) {
-            currentBuzzObject.pause();
-            song.playing = false;
+             song = song || SongPlayer.currentSong;
+             currentBuzzObject.pause();
+             song.playing = false;
+         };
+
+         SongPlayer.previous = function() {
+             var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+             currentSongIndex--;
+
+             if (currentSongIndex < 0) {
+                 currentBuzzObject.stop();
+                 SongPlayer.currentSong.playing = null;
+             } else {
+                 var song = currentAlbum.songs[currentSongIndex];
+                 setSong(song);
+                 playSong(song);
+             }
          };
 
          return SongPlayer;
