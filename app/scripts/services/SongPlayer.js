88eb19f236c1c57c306c8379b35f9d5de43e73eb
Stutay Monga <Stutay.Monga9@gmail.com>
2017-09-08 15:54:50 -0400

    extra credit

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -29,6 +29,10 @@
              currentBuzzObject.bind('timeupdate', function() {
                  $rootScope.$apply(function() {
                      SongPlayer.currentTime = currentBuzzObject.getTime();
+                        var duration = currentBuzzObject.getDuration();
+                          if (SongPlayer.currentTime === duration) {
+                            SongPlayer.next()
+                          }
                  });
              });
 
